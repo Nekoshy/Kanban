63815b216ad000c2d7d01ca947066b28b1262ace
Nekoshy <neko@intmail.pl>
2021-03-21 17:25:25 +0100

    dodawanie zadan przy pomocy guzika / nie mozna jeszcze ustawic tresci zadania

--- app/src/main/java/com/example/kanban/MainActivity.kt
+++ app/src/main/java/com/example/kanban/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.kanban
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,14 +16,17 @@ class MainActivity : AppCompatActivity() {
 
         val arrayAdapter: ArrayAdapter<*>
         val zadania1: MutableList<String> = ArrayList()
-        zadania1.add("Zadanie1")
-        zadania1.add("Zadanie2")
-        zadania1.add("Zadanie3")
 
         val mListView1 = findViewById<ListView>(R.id.listview1)
 
         arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, zadania1)
         mListView1.adapter = arrayAdapter
+
+        val btnToDo = findViewById(R.id.btDoZrobienia) as Button
+        btnToDo.setOnClickListener {
+            zadania1.add("Zadanie")
+            mListView1.adapter = arrayAdapter
+        }
 
 
     }

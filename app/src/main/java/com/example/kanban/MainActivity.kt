e0fc189df01821fc7a02ad071a70bf0daea1110f
Nekoshy <neko@intmail.pl>
2021-03-21 16:30:17 +0100

    lista zadan / jeszcze bez dodawania nowych przy pomocy guzika

--- app/src/main/java/com/example/kanban/MainActivity.kt
+++ app/src/main/java/com/example/kanban/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.kanban
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val arrayAdapter: ArrayAdapter<*>
+        val zadania1: MutableList<String> = ArrayList()
+        zadania1.add("Zadanie1")
+        zadania1.add("Zadanie2")
+        zadania1.add("Zadanie3")
+
+        val mListView1 = findViewById<ListView>(R.id.listview1)
+
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, zadania1)
+        mListView1.adapter = arrayAdapter
+
+
     }
 }
